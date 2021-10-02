08b4d3ea65354c1a4750081b009199aa3357cac7
Nicoli Félix <nicolijfelix@gmail.com.br>
2021-10-02 19:03:46 -0300

    fix slides e responsivo

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,38 +1,19 @@
+const swiper2 = new Swiper(".swiper-banner", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  centeredSlides: true,
+  autoplay: 1000,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
 
 const swiper = new Swiper(".swiper-container", {
     slidesPerView: 4,
     slidesPerGroup: 4,
-    
-    autoplay: 1500,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-     breakpoints: {
-      // when window width is >= 600px
-      600: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 5,
-        centeredSlides: true
-        
-      },
-       // when window width is >= 900px
-       900: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 5,
-         centeredSlides: false
-      },
-    }
-  });
-
-
-  
-  const swiper2 = new Swiper(".swiper-banner", {
-    slidesPerView: 1,
-    slidesPerGroup: 1,
     centeredSlides: true,
     autoplay: 1500,
     loop: true,
@@ -40,10 +21,15 @@ const swiper = new Swiper(".swiper-container", {
       el: ".swiper-pagination",
       clickable: true,
     },
-     
-       // when window width is >= 900px
-       
-    
+     breakpoints: {
+       // when window width is >= 800px
+       800: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 5,
+         centeredSlides: false,
+      },
+    }
   });
 
 
@@ -60,8 +46,8 @@ const swiper = new Swiper(".swiper-container", {
      breakpoints: {
       // when window width is >= 600px
       600: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
         spaceBetween: 5,
         centeredSlides: true
         
@@ -75,8 +61,6 @@ const swiper = new Swiper(".swiper-container", {
       },
     }
   });
-
-  
 
   
 
